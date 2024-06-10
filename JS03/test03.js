8227e97667b39bcb0cd3539ce949cd497f7a6859
ShuttlerBad <dileepasudara@gmail.com>
2024-06-10 19:07:31 +0530

    Added the ForLoop

--- JS03/test03.js
+++ JS03/test03.js
@@ -1,3 +1,4 @@
+//WhileLoop
 var age = 5;
 
 while (age < 10){
@@ -5,3 +6,10 @@ while (age < 10){
     age++;
 }
 document.write("You're now over 10!");
+
+
+//ForLoop
+for( Age=10; Age < 10; Age++){
+    console.log("Your age is less than 10!");
+}
+document.write("You are now over 10");

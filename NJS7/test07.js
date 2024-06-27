3c387547a2b6052f50713526aebc03def3718b3d
ShuttlerBad <dileepasudara@gmail.com>
2024-06-27 21:52:10 +0530

    Using Scope

---
+++ NJS7/test07.js
@@ -0,0 +1,20 @@
+
+var color = "purple";
+
+document.querySelector(".left").style.backgroundColor = color;
+document.querySelector(".left .color-value").innerHTML = color;
+
+var color = "skyblue";
+
+//this function call heading color sets blue
+function headingColor() {
+    color = "blue";
+    document.querySelector(".title").style.color = color;
+  }
+ 
+
+
+document.querySelector(".right").style.backgroundColor = color;
+document.querySelector(".right .color-value").innerHTML = color;
+
+headingColor();
